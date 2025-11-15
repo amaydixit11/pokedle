a041117626ff68ac7d9446a5ae2ace74eadfdbec
amaydixit11 <amaydixit11@gmail.com>
2025-11-16 01:24:26 +0530

    added api

--- frontend/components/main.tsx
+++ frontend/components/main.tsx
@@ -12,7 +12,7 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
-const API_URL = process.env.API_URL || "http://localhost:8000";
+const API_URL = process.env.API_URL;
 
 interface Config {
   attributes: string[];
